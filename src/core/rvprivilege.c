d94949a0c79064e6bc0372e1c9f7fc5f7732ab09
charain <charain_li@outlook.com>
2024-12-29 19:56:13 +0800

    添加csr读写函数

--- src/core/rvprivilege.c
+++ src/core/rvprivilege.c
@@ -1,5 +1,16 @@
 #include "core/rvcore_priv.h"
 
-u64 riscv64_csr_read(struct RiscvCore64 *core, u16 addr) { return core->csrs[addr]; }
+u64 riscv64_csr_read(struct RiscvCore64 *core, u16 addr) {
+    if (addr == SIE) {
+        return core->csrs[MIE] & core->csrs[MIDELEG];
+    }
+    return core->csrs[addr];
+}
 
-void riscv64_csr_write(struct RiscvCore64 *core, u16 addr, u64 value) { core->csrs[addr] = value; }
+void riscv64_csr_write(struct RiscvCore64 *core, u16 addr, u64 value) {
+    if (addr == SIE) {
+        core->csrs[MIE] = (core->csrs[MIE] & ~core->csrs[MIDELEG]) | (value & core->csrs[MIDELEG]);
+    } else {
+        core->csrs[addr] = value;
+    }
+}
