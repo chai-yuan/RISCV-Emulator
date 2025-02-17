534580fb9d0b61e100aca33546feaf2c05bb6806
charain <charain_li@outlook.com>
2025-02-17 10:46:16 +0800

    完善uart16550实现

--- include/device/uart16550.h
+++ include/device/uart16550.h
@@ -10,21 +10,27 @@ typedef void (*put_char_func_t)(u8 data);
 
 #define UART_SIZE (0x8)
 #define UART_THR (0) // TX
-#define UART_RHR (0) // RX
+#define UART_RHR (0) // RX 这里直接使用RX充当接收缓冲区
 #define UART_IER (1)
 #define UART_IIR (2)
 #define UART_FCR (2)
+#define UART_LCR (3)
 #define UART_LSR (5)
-#define UART_LSR (5)
+#define UART_MSR (6)
+#define UART_SCR (7)
 // 线路状态寄存器标志位
-#define UART_LSR_RX_EMPTY (1 << 0)
+#define UART_IER_RDA (1)
+#define UART_IER_THRE (1 << 1)
+#define UART_FCR_CLEAR (1 << 1)
+#define UART_LCR_DIVREG (1 << 7)
+#define UART_LSR_DATA_READY (1 << 0)
 #define UART_LSR_TX_EMPTY (1 << 5)
 #define UART_LSR_THR_SR_EMPTY (1 << 6)
 
 struct UART16550 {
-    u8   data[UART_SIZE]; // 模拟 UART 寄存器
-    u32  last_update;
-    bool interrupt;
+    u8  data[UART_SIZE]; // 模拟 UART 寄存器
+    u8  divregs[2];      // 除数寄存器
+    u32 last_update;
 
     get_char_func_t get_char; // 外部提供的输入函数
     put_char_func_t put_char; // 外部提供的输出函数
--- src/device/uart16550.c
+++ src/device/uart16550.c
@@ -3,19 +3,14 @@
 #include "types.h"
 
 void uart_init(struct UART16550 *uart, get_char_func_t get, put_char_func_t put) {
-    uart->get_char  = get;
-    uart->put_char  = put;
-    uart->interrupt = false;
-    uart->data[UART_LSR] |= (UART_LSR_TX_EMPTY | UART_LSR_THR_SR_EMPTY);
+    uart->get_char       = get;
+    uart->put_char       = put;
+    uart->data[UART_LCR] = 0x3;
+    uart->data[UART_LSR] = UART_LSR_TX_EMPTY | UART_LSR_THR_SR_EMPTY;
+    uart->data[UART_MSR] = (1 << 7) | (1 << 4);
 }
 
-bool uart_check_irq(struct UART16550 *uart) {
-    if (uart->interrupt) {
-        uart->interrupt = false;
-        return true;
-    }
-    return false;
-}
+bool uart_check_irq(struct UART16550 *uart) { return !(uart->data[UART_IIR] & 1); }
 
 static enum exception uart_read(void *context, u64 addr, u8 size, usize *data) {
     struct UART16550 *uart = (struct UART16550 *)context;
@@ -24,8 +19,20 @@ static enum exception uart_read(void *context, u64 addr, u8 size, usize *data) {
 
     switch (addr) {
     case UART_RHR:
-        *data = uart->data[UART_RHR];
-        uart->data[UART_LSR] &= ~UART_LSR_RX_EMPTY;
+        if (uart->data[UART_LCR] & UART_LCR_DIVREG) { // 除数寄存器
+            *data = uart->divregs[0];
+        } else {
+            *data = uart->data[UART_RHR];
+            uart->data[UART_LSR] &= ~UART_LSR_DATA_READY;
+            uart->data[UART_RHR] = 0;
+        }
+        break;
+    case UART_IER:
+        if (uart->data[UART_LCR] & UART_LCR_DIVREG) { // 除数寄存器
+            *data = uart->divregs[1];
+        } else {
+            *data = uart->data[UART_IER];
+        }
         break;
     default:
         *data = uart->data[addr];
@@ -38,14 +45,36 @@ static enum exception uart_read(void *context, u64 addr, u8 size, usize *data) {
 static enum exception uart_write(void *context, u64 addr, u8 size, usize data) {
     struct UART16550 *uart = (struct UART16550 *)context;
     if (size != 1 || addr > UART_SIZE)
-        return LOAD_ACCESS_FAULT;
+        return STORE_AMO_ACCESS_FAULT;
 
     switch (addr) {
     case UART_THR:
-        if (uart->put_char)
-            uart->put_char((u8)data);
-        uart->data[UART_LSR] |= UART_LSR_TX_EMPTY;
+        if (uart->data[UART_LCR] & UART_LCR_DIVREG) { // 除数寄存器
+            uart->divregs[0] = data;
+        } else {
+            if (uart->put_char)
+                uart->put_char((u8)data);
+            uart->data[UART_LSR] |= UART_LSR_TX_EMPTY;
+        }
         break;
+    case UART_IER:
+        if (uart->data[UART_LCR] & UART_LCR_DIVREG) { // 除数寄存器
+            uart->divregs[1] = data;
+        } else {
+            uart->data[UART_IER] = data;
+        }
+        break;
+    case UART_FCR:
+        if (data & UART_FCR_CLEAR) { // 清空读入数据
+            uart->data[UART_LSR] &= ~UART_LSR_DATA_READY;
+            uart->data[UART_RHR] = 0;
+        }
+        break;
+
+        // 不允许写入
+    case UART_LSR:
+    case UART_MSR:
+        return STORE_AMO_ACCESS_FAULT;
     default:
         uart->data[addr] = (u8)data;
         break;
@@ -56,7 +85,16 @@ static enum exception uart_write(void *context, u64 addr, u8 size, usize data) {
 
 static void uart_update(void *context, u32 interval) {
     struct UART16550 *uart = (struct UART16550 *)context;
+    // 更新中断
+    uart->data[UART_IIR] = (0x3 << 6);
+    u8 no_int            = 1;
+    if ((uart->data[UART_IER] & UART_IER_RDA) && (uart->data[UART_LSR] & UART_LSR_DATA_READY))
+        uart->data[UART_IIR] |= (1 << 2), no_int = 0;
+    if ((uart->data[UART_IER] & UART_IER_THRE) && (uart->data[UART_LSR] & UART_LSR_THR_SR_EMPTY))
+        uart->data[UART_IIR] |= (1 << 1), no_int = 0;
+    uart->data[UART_IIR] |= no_int;
 
+    // 检查输入
     uart->last_update += interval;
     if (!uart->get_char || uart->last_update < 20000)
         return;
@@ -64,11 +102,9 @@ static void uart_update(void *context, u32 interval) {
 
     u8 input_char;
     if (uart->get_char(&input_char)) {
-        if (!(uart->data[UART_LSR] & UART_LSR_RX_EMPTY)) {
+        if (!(uart->data[UART_LSR] & UART_LSR_DATA_READY)) {
             uart->data[UART_RHR] = input_char;
-            uart->data[UART_LSR] |= UART_LSR_RX_EMPTY;
-
-            uart->interrupt = true;
+            uart->data[UART_LSR] |= UART_LSR_DATA_READY;
         }
     }
 }
